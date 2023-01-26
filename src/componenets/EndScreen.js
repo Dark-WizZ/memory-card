abc5c731c59f950fd2b955f0005126a25077ae2f
Dark-WizZ <Dark-WizZ>
2023-01-26 07:49:23 +0530

    Spelling fix on end screen

--- src/componenets/EndScreen.js
+++ src/componenets/EndScreen.js
@@ -4,7 +4,6 @@ function EndScreen(props){
   return<div className="end_screen">
     <Scores score={props.score} hiScore={props.hiScore} />
     <Greeting isWon={props.isWon} />
-    <div>You clicked same emoji twice!</div>
     <button className='replay_btn' onClick={props.replayClk}>Wanna Replay?</button>
   </div>
 }
@@ -12,9 +11,11 @@ function EndScreen(props){
 function Greeting(props){
   return (props.isWon)? <div className='greet won'>
     Big Brain, you Won!...
+    <div>congratulation, you have collected all the emoji power...</div>
   </div>:
   <div className="greet lose">
     You lose dumbass...
+    <div>You clicked same emoji twice!</div>
   </div>
 }
 
